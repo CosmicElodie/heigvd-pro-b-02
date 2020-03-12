fd0f24175b907b6f464d3ac65c598ae1d1e382ff
mikeDTr <61268556+mikeDTr@users.noreply.github.com>
2020-03-12 21:54:33 +0100

    Removed request mapping

--- api/src/main/java/controllers/AuthenticationController.java
+++ api/src/main/java/controllers/AuthenticationController.java
@@ -71,21 +71,6 @@ public class AuthenticationController {
       return responseObject.toString();
     }
 
-    @RequestMapping(value = {"/logout"}, method = RequestMethod.POST)
-    public String logoutDo(HttpServletRequest request,HttpServletResponse response){
-        HttpSession session= request.getSession(false);
-        SecurityContextHolder.clearContext();
-        session= request.getSession(false);
-        if(session != null) {
-            session.invalidate();
-        }
-        for(Cookie cookie : request.getCookies()) {
-            cookie.setMaxAge(0);
-        }
-
-        return "logout";
-    }
-
     @GetMapping("/authentication/user")
     public String currentUserName(Principal principal) {
         return principal.getName();
