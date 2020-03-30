338c77137b5c1bf4140d2964234fe5fd413e9f23
AliceThunderWind <dalia.maillefer@gmail.com>
2020-03-30 15:15:09 +0200

    Edited ForumController

--- api/src/main/java/controllers/ForumController.java
+++ api/src/main/java/controllers/ForumController.java
@@ -2,28 +2,47 @@ package controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
 
 @RestController
 public class ForumController {
     @Autowired
     private DataSource dataSource;
 
-    private Connection mConnection;
-
     @GetMapping("/forum/all")
     public String forumList() throws SQLException {
 
+        String result = null;
+
         try (Connection conn = dataSource.getConnection()) {
+            Statement stmt = conn.createStatement();
+            ResultSet forums = stmt.executeQuery("select DEV.getForums() AS forum_result");
 
+            forums.next();
+            result = forums.getString("forum_result");
         }
+        return result;
+    }
 
 
-        return null;
+    @GetMapping("/forum/user")
+    public String fetchMe(@RequestParam("user_id") int user_id) throws SQLException {
+
+        String result = null;
+
+        try (Connection conn = dataSource.getConnection()) {
+            Statement stmt = conn.createStatement();
+            ResultSet forums = stmt.executeQuery("select DEV.getUserJSON(" + user_id + ") AS forum_result");
+
+            forums.next();
+            result = forums.getString("forum_result");
+        }
+        return result;
     }
 
 }
