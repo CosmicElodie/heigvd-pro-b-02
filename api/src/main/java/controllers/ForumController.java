52819be912d9a10df4364bd1b147a50f189ed3e2
AliceThunderWind <dalia.maillefer@gmail.com>
2020-03-30 14:35:06 +0200

    Added ForumController

--- api/src/main/java/controllers/ForumController.java
+++ api/src/main/java/controllers/ForumController.java
@@ -1,4 +1,29 @@
 package controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@RestController
 public class ForumController {
+    @Autowired
+    private DataSource dataSource;
+
+    private Connection mConnection;
+
+    @GetMapping("/forum/all")
+    public String forumList() throws SQLException {
+
+        try (Connection conn = dataSource.getConnection()) {
+
+        }
+
+
+        return null;
+    }
+
 }
