0fe7dadec254d77565413f3b6d9e1302325383f6
MikeDTr <michael.triponez@heig-vd.ch>
2020-04-01 13:33:55 +0200

    Profile modal to dialog

--- web/src/pages/profile/ModalProfile.js
+++ web/src/pages/profile/ModalProfile.js
@@ -1,70 +1,234 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
+import React, {useContext} from 'react';
+import { makeStyles, Card, CardActions, CardContent, Button, Typography, Avatar, Grid } from '@material-ui/core';
+import { blue, blueGrey, HUE } from '@material-ui/core/colors';
+import EditIcon from '@material-ui/icons/Edit';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import { MainContext } from '../../context/MainContext';
+import { FormControlLabel, TextField, Checkbox, CssBaseline, Link, Paper, Box, Icon } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
 
-function rand() {
-  return Math.round(Math.random() * 20) - 10;
-}
 
-function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
+const useStyles = makeStyles(theme => ({
+  bannerBox: {
+    minWidth: 275,
+    Height: 300 
+  },
+  InfoBox: {
+    marginTop: 20,
+    maxWidth: 400,
+    Height: 300 
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    marginLeft: 10,
+    fontSize: 30,
+    color: blueGrey[50],
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  resize: {
+    scale : 2,
+  },
+  large: {
+    width: theme.spacing(20),
+    height: theme.spacing(20),   
+  },
+  contour: {
+    marginTop: 0,
+    width: theme.spacing(21),
+    height: theme.spacing(21),
+    backgroundColor: '#ffffff',  
+  },
+  house:{
+    backgroundImage :  `url('https://upload.wikimedia.org/wikipedia/commons/4/48/Epcot_Page_Banner.jpg')`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    alignItems: 'center',
+  },
+  banner:{
+    height: 200,
+    width:250,
+    alignItems: 'right',
+  },
 
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
-  };
-}
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
+  wrapper: {
+    position: 'relative',
+  },
+  div: {
     position: 'absolute',
-    width: 400,
+    top: 28,
+    right: 0,
+    left: 0,
+    border: '1px solid',
+    padding: theme.spacing(1),
     backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
   },
 }));
 
-export default function SimpleModal() {
-  const classes = useStyles();
-  // getModalStyle is not a pure function, we roll the style only on the first render
-  const [modalStyle] = React.useState(getModalStyle);
-  const [open, setOpen] = React.useState(false);
+const USER_DATA = {
+  name: 'Michael Triponez',
+  img: 'https://www.cdc.gov/coronavirus/2019-ncov/images/2019-coronavirus.png',
+  username: 'Mich'
+}
+const HOUSE_DATA = {
+  name: 'Michael Triponez',
+  img : 'https://c1-ebgames.eb-cdn.com.au/merchandising/images/packshots/594355e64e564fb6bdcc760f8e2cc8e6_Large.png',
+  username: 'Mich'
+}
 
-  const handleOpen = () => {
-    setOpen(true);
+
+export default function Profile() {
+  const { user, setDialog } = useContext(MainContext);
+  const classes = useStyles();
+  const SPACING = 3;
+  const [open, setOpen] = React.useState(false);
+  const bull = <span className={classes.bullet}>•</span>;
+
+  const handleClick = () => {
+    console.log('Le lien a été cliqué.');
+    setOpen(prev => !prev);
   };
 
-  const handleClose = () => {
+  const bla =() =>{
+    setDialog( { show_profile : {
+      is_open: true
+  }});
+  }
+
+  const handleClickAway = () => {
     setOpen(false);
   };
-
-  const body = (
-    <div style={modalStyle} className={classes.paper}>
-      <h2 id="simple-modal-title">Text in a modal</h2>
-      <p id="simple-modal-description">
-        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-      </p>
-      <SimpleModal />
-    </div>
-  );
-
+  
   return (
     <div>
-      <button type="button" onClick={handleOpen}>
-        Open Modal
-      </button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-      >
-        {body}
-      </Modal>
-    </div>
+      <Card className={classes.bannerBox}>
+        <CardContent className={classes.house} >
+          <Grid container spacing={3}>
+            <Grid item xs>
+              <Typography className={classes.title} color="textSecondary" gutterBottom>
+                Profile
+              </Typography>
+
+              <Avatar className={classes.contour} > 
+                <Avatar className={classes.large} > 
+                  <img className={classes.resize} src={USER_DATA.img}>
+                  </img>
+                </Avatar>
+              </Avatar>
+            </Grid>
+            <Grid item>
+              <img className={classes.banner} src={HOUSE_DATA.img}>
+              </img>
+            </Grid>
+            
+          </Grid>
+
+        </CardContent>
+  
+        <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+                  onClick= { bla }
+                >
+                  bla
+                </Button>
+      </Card>
+        <Card className={classes.InfoBox}>
+          <CardContent >
+             
+                        <CssBaseline />
+                        <Grid className={classes.grid_container} item component={Paper} elevation={0}>
+                          <div className={classes.paper}>
+                            
+                            
+                            
+                            <Typography component="h1" variant="h4"spacing={10}>
+                              Info
+                            </Typography>
+
+                            <br />
+                            <br />
+
+                          <Typography component="h1" >
+                            <Grid container spacing={SPACING}>
+                                <Grid item xs>
+                                <b>Nom :</b>  
+                                </Grid>
+                                <Grid item>
+                                  <span >{user.lastname}</span>
+                                </Grid>
+                              </Grid>
+                              
+                              <Grid container spacing={SPACING}>
+                                <Grid item xs>
+                                <b>Prenom :</b>
+                                </Grid>
+                                <Grid item>
+                                 <span >{user.firstname}</span>
+                                </Grid>
+                              </Grid>
+                              
+                              <Grid container spacing={SPACING}>
+                                <Grid item xs>
+                                <b>Birthdate :</b>
+                                </Grid>
+                                <Grid item>
+                                <span >{user.created}</span>              
+                                </Grid>
+                              </Grid>
+                              
+                              <Grid container spacing={SPACING}>
+                                <Grid item xs>
+                                <b>Maison :</b> 
+                                </Grid>
+                                <Grid item>
+                                <span >{user.house && user.house.name} </span>
+                                </Grid>
+                              </Grid>
+                              
+                              <Grid container spacing={SPACING}>
+                                <Grid item xs>
+                                <b>Username :</b>  
+                                </Grid>
+                                <Grid item>
+
+                                  
+                                <IconButton aria-label="edit"  size="small">
+                                  <EditIcon style={{ fontSize: 10, color: blue[500] }} onClick={handleClick}/>  
+                                </IconButton>
+                                
+                                <span >{user.username}</span> 
+                                </Grid>
+                              </Grid>
+
+                              <Grid container spacing={SPACING}>
+                                <Grid item xs>
+                                <b>email :</b>
+                                </Grid>
+                                <Grid item>
+                                <span >{user.email}</span>
+                                </Grid>
+                              </Grid>
+                            </Typography>
+                          
+                       
+                          </div>
+                        </Grid>
+                      
+                 
+
+
+        </CardContent>
+      </Card>
+  </div>
   );
 }
