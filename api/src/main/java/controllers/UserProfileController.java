0ee22f59c41331373927c494b78d77f1edd12159
JunodChristophe <49147076+JunodChristophe@users.noreply.github.com>
2020-04-04 17:30:21 +0200

    update errors insertUser in UserProfileController

--- api/src/main/java/controllers/UserProfileController.java
+++ api/src/main/java/controllers/UserProfileController.java
@@ -45,46 +45,40 @@ public class UserProfileController {
         JsonObjectBuilder responseObject = Json.createObjectBuilder();
 
         String pattern = "(\\w|-)+\\.(\\w|-)+@heig-vd\\.ch";
-        if (!email.matches(pattern)) {
+        if(firstname.isEmpty() || lastname.isEmpty() ||
+                username.isEmpty() || password.isEmpty() || !email.matches(pattern)) {
+
             responseObject.add("status", "error");
-            responseObject.add("dialog_id", "invalid_email_syntax");
-            return responseObject.build().toString();
-        }
-
-//        if (firstname.length() == 0 || lastname.length() == 0 ||
-//                username.length() == 0 || password.length() == 0) {
-//            responseObject.add("status", "error");
-//            responseObject.add("dialog_id", "empty_field");
-//            return responseObject.build().toString();
-//        }
-
-
-        if(firstname == "" ||  lastname == "" || username == "" || password == "") {
-            responseObject.add("status", "errorEmptyField");
-            if (firstname == "") {
+            if (!email.matches(pattern)) {
+                responseObject.add("dialog_id", "invalid_email_syntax");
+            }
+            if (firstname.isEmpty()) {
                 responseObject.add("empty1", "empty_firstname");
             }
-            if (lastname == "") {
+            if (lastname.isEmpty()) {
                 responseObject.add("empty2", "empty_lastname");
             }
-            if (username == "") {
+            if (username.isEmpty()) {
                 responseObject.add("empty3", "empty_username");
             }
-            if (password == "") {
+            if (password.isEmpty()) {
                 responseObject.add("empty4", "empty_password");
             }
+
             return responseObject.build().toString();
         }
-
 
         try (Connection conn = dataSource.getConnection()) {
 
             // Test si l'email existe déjà dans la base de donnée.
-//            if (conn.createStatement().executeQuery("SELECT user_id FROM user WHERE email = " + email).next()) {
-//                responseObject.add("status", "error");
-//                responseObject.add("dialog_id", "email_already_exist");
-//                return responseObject.build().toString();
-//            }
+            if (conn.createStatement().executeQuery(
+                    "SELECT user_id FROM user WHERE email LIKE '" + email + "';"
+                    ).next()) {
+
+                responseObject.add("status", "error");
+                responseObject.add("dialog_id", "email_already_exist");
+                return responseObject.build().toString();
+            }
 
             CallableStatement insertionUser = conn.prepareCall("{CALL insertUser(?,?,?,?,?,?,?)}");
             insertionUser.setDate(1, birth);
