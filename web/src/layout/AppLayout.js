d38ec80977ca7cafc3bb916514fc2a922fb832fc
Ovich <stefanteofanovic@hotmail.com>
2020-04-07 05:22:52 +0200

    EventProvider -> Envlopper les components Event dans AppLayout

--- web/src/layout/AppLayout.js
+++ web/src/layout/AppLayout.js
@@ -1,18 +1,19 @@
-import React from 'react'
-import Menu from './Menu'
-import User from './User'
-import Dialog from './Dialog'
-import ShowProfile from './ShowProfile'
-import Home from '../pages/Home'
-import About from '../pages/About'
-import Event_Welcome from '../pages/event/Event_Welcome'
-import Event_Create from '../pages/event/Event_Create'
-import Event_Calendar from '../pages/event/Event_Calendar'
-import Profile from '../pages/profile/Profile'
-import ModalProfile from '../pages/profile/ModalProfile'
-import Forum from '../pages/forum/Forum'
+import React from 'react';
+import Menu from './Menu';
+import User from './User';
+import Dialog from './Dialog';
+import ShowProfile from './ShowProfile';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Event_Welcome from '../pages/event/Event_Welcome';
+import Event_Create from '../pages/event/Event_Create';
+import Event_Calendar from '../pages/event/Event_Calendar';
+import Profile from '../pages/profile/Profile';
+import ModalProfile from '../pages/profile/ModalProfile';
+import Forum from '../pages/forum/Forum';
 import { ForumProvider } from '../context/ForumContext';
-import { Switch, Route } from "react-router-dom"
+import { EventProvider } from '../context/EventContext';
+import { Switch, Route } from "react-router-dom";
 
 import '../css/App.css'; 
 import '../css/typo.css'; 
@@ -30,10 +31,11 @@ const AppLayout = () => {
             <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/about" component={About} />
-           
-            <Route path="/event_welcome" component={Event_Welcome} />
-            <Route path="/event_create" component={Event_Create} />
-            <Route path="/event_calendar" component={Event_Calendar} />
+            <EventProvider>
+                <Route path="/event_welcome" component={Event_Welcome} />
+                <Route path="/event_create" component={Event_Create} />
+                <Route path="/event_calendar" component={Event_Calendar} />
+            </EventProvider>
             <Route path="/profile" component={Profile} />
             <Route path="/test" component={ModalProfile} />
             <ForumProvider>
