4b8ed0a7f232a3bca2728d02885de073ac784163
CosmicElodie <CosmicElodie@users.noreply.github.com>
2020-04-16 22:44:17 +0200

    sorting presque fini. Il manque juste le map de classement pour pouvoir sort

--- web/src/pages/event/Event_List.js
+++ web/src/pages/event/Event_List.js
@@ -1,37 +1,28 @@
 import React, {useContext, useEffect} from 'react';
 import {MainContext} from '../../context/MainContext';
 import { EventContext } from '../../context/EventContext';
+import Typography from '@material-ui/core/Typography';
+
+import PropTypes from "prop-types";
 
 import {Button, 
     Card, 
     CardActions, 
     CardContent, 
     CardMedia, 
-    Checkbox,
-    clsx,
     CssBaseline, 
-    DeleteIcon,
-    FilterListIcon,
-    FormControlLabel,
     Grid, 
-    IconButton,
-    lighten,
-    Paper,
-    PropTypes,
-    Switch,
     Table, 
     TableBody, 
     TableCell, 
-    TableContainer,
-    TableHead,  
-    TablePagination,
+    TableHead, 
+    TableContainer, 
+    TableRow,
     TableSortLabel,
-    TableRow, 
-    Toolbar,
-    Tooltip,
-    Typography} from '@material-ui/core';
+    Paper} from '@material-ui/core';
 
-import {makeStyles, withStyles} from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
+
 
 const useStyles = makeStyles(theme => ({
     card: { //dans la carte
@@ -59,51 +50,99 @@ function descendingComparator(a, b, orderBy) {
       return 1;
     }
     return 0;
-  }
-
-  function getComparator(order, orderBy) {
-    return order === 'desc'
+}
+  
+function getComparator(order, orderBy) {
+    return order === "desc"
       ? (a, b) => descendingComparator(a, b, orderBy)
       : (a, b) => -descendingComparator(a, b, orderBy);
-  }
-  
-  function stableSort(array, comparator) {
+}
+
+function stableSort(array, comparator) {
     const stabilizedThis = array.map((el, index) => [el, index]);
     stabilizedThis.sort((a, b) => {
       const order = comparator(a[0], b[0]);
       if (order !== 0) return order;
       return a[1] - b[1];
     });
-    return stabilizedThis.map((el) => el[0]);
-  }
+    return stabilizedThis.map(el => el[0]);
+}
+
+//Titre des colonnes
+const headCells = [
+    { id: 'name', numeric: false, disablePadding: false, label: 'Nom' },
+    { id: 'organisator', numeric: false, disablePadding: false, label: 'Organisateur' },
+    { id: 'limitation', numeric: false, disablePadding: false, label: 'Limitation' },
+    { id: 'nbAttendees', numeric: true, disablePadding: false, label: 'Nb participants' },
+    { id: 'limit_date', numeric: false, disablePadding: false, label: 'Date limite inscription' },
+    { id: 'creation_date', numeric: false, disablePadding: false, label: 'Date' },
+    { id: 'location', numeric: false, disablePadding: false, label: 'Lieu' }
+];
+
+function EnhancedTableHead(props) 
+{
+    const { classes, order, orderBy, onRequestSort } = props;
+    const createSortHandler = property => event => {
+      onRequestSort(event, property);
+    };
+    return (
+        <TableHead>
+          <TableRow>
+            {headCells.map(headCell => (
+              <TableCell
+                key={headCell.id}
+                align={headCell.numeric ? "right" : "left"}
+                sortDirection={orderBy === headCell.id ? order : false}
+              >
+                <TableSortLabel
+                  active={orderBy === headCell.id}
+                  direction={orderBy === headCell.id ? order : "asc"}
+                  onClick={createSortHandler(headCell.id)}
+                >
+                  {headCell.label}
+                </TableSortLabel>
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+    );
+}
+
+EnhancedTableHead.propTypes = {
+    classes: PropTypes.object.isRequired,
+    onRequestSort: PropTypes.func.isRequired,
+    order: PropTypes.oneOf(["asc", "desc"]).isRequired,
+    orderBy: PropTypes.string.isRequired,
+    rowCount: PropTypes.number.isRequired
+};
 
 export default function Event_List() {
-    const {user:{event}} = useContext(MainContext);
-
-    const {data, setData} = useContext(EventContext);
-    
-    useEffect(() => {console.log('TEST : ' + event);}, [event]);
     const classes = useStyles();
-    
-    /*
-    function choosePuce(house)
-    {
-        switch(house)
-        {
-            case 5 : return "./puce_ID.png";
-                break;
-            case 4 : return "./puce_IL.png";
-                break;
-            case 1 : return "./puce_IE.png";
-                break;
-            case 2 : return "./puce_TS.png";
-                break;
-            case 3 : return "./puce_TR.png";
-                break;
-            default : return "";
-        }
-    } */
+    const {user:{event}} = useContext(MainContext);
+    const {data, setData} = useContext(EventContext);
 
+    const [order, setOrder] = React.useState("asc");
+    const [orderBy, setOrderBy] = React.useState("calories");
+    const [selected, setSelected] = React.useState([]);
+    const [page, setPage] = React.useState(0);
+    const [dense, setDense] = React.useState(false);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+    const handleRequestSort = (event, property) => {
+        const isAsc = orderBy === property && order === "asc";
+        setOrder(isAsc ? "desc" : "asc");
+        setOrderBy(property);
+    };
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+    
+    const handleChangeRowsPerPage = event => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+    
     function displayRightHouse(noHouse)
     {
         switch(noHouse)
@@ -132,33 +171,35 @@ export default function Event_List() {
                         <CardContent className={classes.cardContent}>
                             <center><h1>Liste des événements</h1></center>
                             <TableContainer component={Paper}>
-                                <Table className={classes.table} aria-label="simple table">
-                                    <TableHead>
-                                        <TableRow>
-                                            <TableCell align="right">Nom</TableCell>
-                                            <TableCell align="right">Organisateur</TableCell>
-                                            <TableCell align="right">Limitation</TableCell>
-                                            <TableCell align="right">Nb participants</TableCell>
-                                            <TableCell align="right">Date limite d'inscription</TableCell>
-                                            <TableCell align="right">Date</TableCell>
-                                            <TableCell align="right">Lieu</TableCell>
-                                        </TableRow>
-                                    </TableHead>
+                                <Table  className={classes.table}
+                                        aria-labelledby="tableTitle"
+                                        size={dense ? "small" : "medium"}
+                                        aria-label="enhanced table"
+                                >
+                                    <EnhancedTableHead
+                                        classes={classes}
+                                        numSelected={selected.length}
+                                        order={order}
+                                        orderBy={orderBy}
+                                        onRequestSort={handleRequestSort}
+                                        rowCount={7}
+                                    />
                                     <TableBody>
-                                    {data && data.map(({ name, price, status, address, created, date_end, event_id, house_id, location, date_begin, difficulty, house_name, description, organisator, battleroyale, nb_attendees, participants, attendees_max, attendees_min, is_competitive, deadline_reservation }) =>
-                                        <TableRow>
-                                            <TableCell align="right">{name}</TableCell>
-                                            <TableCell align="right">
-                                                {/*<img src={require(choosePuce(user.house && user.house.name))}/> */}
-                                                {organisator.firstname + ' ' + organisator.lastname + ' '}
-                                                <img src={require('./puce_IL.png')}/>
-                                            </TableCell>
-                                            <TableCell align="right">{displayRightHouse(house_id)}</TableCell>
-                                            <TableCell align="right">{nb_attendees + ' / ' + attendees_max}</TableCell>
-                                            <TableCell align="right">{deadline_reservation}</TableCell>
-                                            <TableCell align="right">{date_begin}</TableCell>
-                                            <TableCell align="right">{location}</TableCell>
-                                        </TableRow>
+                                    {data && data.map(({ name, price, status, address, created, date_end, event_id, house_id, location, date_begin, difficulty, house_name, description, organisator, battleroyale, nb_attendees, participants, attendees_max, attendees_min, is_competitive, deadline_reservation }, index) =>
+                                                <TableRow tabIndex={-1} key={name}>
+                                                    <TableCell component="th"
+                                                        id={`enhanced-table-checkbox-${index}`}
+                                                        scope="row">{name}</TableCell>
+                                                    <TableCell align="right">
+                                                        {/*<img src={require(choosePuce(user.house && user.house.name))}/> */}
+                                                        {organisator.firstname + ' ' + organisator.lastname + ' '}<img src={require('./puce_IL.png')}/>
+                                                    </TableCell>
+                                                    <TableCell align="right">{displayRightHouse(house_id)}</TableCell>
+                                                    <TableCell align="right">{nb_attendees + ' / ' + attendees_max}</TableCell>
+                                                    <TableCell align="right">{deadline_reservation}</TableCell>
+                                                    <TableCell align="right">{date_begin}</TableCell>
+                                                    <TableCell align="right">{location}</TableCell>
+                                                </TableRow>
                                     )}
                                     </TableBody>
                                 </Table>
