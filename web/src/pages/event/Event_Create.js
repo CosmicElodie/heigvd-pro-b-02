c289a9a93a8231a58f6b3abf197168bb14d5b0c1
CosmicElodie <CosmicElodie@users.noreply.github.com>
2020-04-18 01:30:37 +0200

    màj fetch event_create

--- web/src/pages/event/Event_Create.js
+++ web/src/pages/event/Event_Create.js
@@ -62,23 +62,23 @@ export default function Event_Create() {
         someDate: new Date()
     };
 
-    const { value:name,  bind:bindName}  = useInput('');
-    const { value:description,        bind:bindDescription }      = useInput('');
-    const { value:is_competitive,    bind:bindIsCompetitive }  = useInput('');
-    const { value:battleroyal,     bind:bindBattleRoyal }   = useInput('');
-    const { value:difficulty,     bind:bindDifficulty }   = useInput('');
-    const { value:price,        bind:bindPrice}       = useInput('');
-    const { value:attendees_min,        bind:bindAttendeesMin}       = useInput('');
-    const { value:attendees_max,        bind:bindAttendeesMax}       = useInput('');
-    const { value:deadline_reservation,        bind:bindDeadlineReservation}       = useInput('');
-    const { value:date_begin,        bind:bindDateBegin}       = useInput('');
-    const { value:date_end,        bind:bindDateEnd}       = useInput('');
-    const { value:location,        bind:bindLocation}       = useInput('');
-    const { value:road,        bind:bindRoad}       = useInput('');
-    const { value:road_number,        bind:bindRoadNumber}       = useInput('');
-    const { value:postal_code,        bind:bindPostalCode}       = useInput('');
-    const { value:town,        bind:bindTown}       = useInput('');
-    const { value:limitation,        bind:bindLimitation}       = useInput('');
+    const { value:name,                 bind:bindName}                  = useInput('');
+    const { value:description,          bind:bindDescription }          = useInput('');
+    const { value:is_competitive,       bind:bindIsCompetitive }        = useInput('');
+    const { value:battleroyal,          bind:bindBattleRoyal }          = useInput('');
+    const { value:difficulty,           bind:bindDifficulty }           = useInput('');
+    const { value:price,                bind:bindPrice}                 = useInput('');
+    const { value:attendees_min,        bind:bindAttendeesMin}          = useInput('');
+    const { value:attendees_max,        bind:bindAttendeesMax}          = useInput('');
+    const { value:deadline_reservation, bind:bindDeadlineReservation}   = useInput('');
+    const { value:date_begin,           bind:bindDateBegin}             = useInput('');
+    const { value:date_end,             bind:bindDateEnd}               = useInput('');
+    const { value:location,             bind:bindLocation}              = useInput('');
+    const { value:road,                 bind:bindRoad}                  = useInput('');
+    const { value:road_number,          bind:bindRoadNumber}            = useInput('');
+    const { value:postal_code,          bind:bindPostalCode}            = useInput('');
+    const { value:town,                 bind:bindTown}                  = useInput('');
+    const { value:limitation,           bind:bindLimitation}            = useInput('');
     
     
 
@@ -100,7 +100,7 @@ export default function Event_Create() {
         "&address=" + road + road_number + ', ' + postal_code + town +
         "&limitation=" + limitation;
 
-        fetch('http://localhost:8080/event_create', {
+        fetch('http://localhost:8080/event/insert_event', {
                 method: 'POST',
                 credentials: 'include',
                 headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
@@ -131,7 +131,7 @@ export default function Event_Create() {
                                     label="Nom événement"
                                     placeholder="Veuillez indiquer le nom de votre événement."
                                     variant="outlined"
-                                    required = "true" 
+                                    required = {true} 
                                     { ...bindName }
                                 />
                                 <br />
@@ -141,7 +141,7 @@ export default function Event_Create() {
                                     placeholder="En quoi consiste votre événement ?"
                                     multiline
                                     variant="outlined"
-                                    required = "true" 
+                                    required = {true}
                                     style = {{minWidth: 600}}
                                     { ...bindDescription }
                                  />
@@ -152,12 +152,12 @@ export default function Event_Create() {
                                         id="is_competitive" 
                                         label="Type" 
                                         required = "true" 
-                                        defaultValue = "Non-compétitif"
+                                        defaultValue = {0}
                                         style = {{width: 150}}
                                         { ...bindIsCompetitive }
                                         select>
-                                            <MenuItem value="Compétitif">Compétitif</MenuItem>
-                                            <MenuItem value="Non-compétitif">Non-compétitif</MenuItem>
+                                            <MenuItem value={1}>Compétitif</MenuItem>
+                                            <MenuItem value={0}>Non-compétitif</MenuItem>
                                         </TextField>
                                     </Grid>
 
@@ -165,13 +165,13 @@ export default function Event_Create() {
                                         <TextField 
                                         id="limitation" 
                                         label="Limitation" 
-                                        required = "true" 
-                                        defaultValue = "Global"
+                                        required = {true} 
+                                        defaultValue = {0}
                                         style = {{width: 150}}
                                         { ...bindLimitation } 
                                         select>
-                                             <MenuItem value= "Maison"> {user.house && user.house.name}</MenuItem>
-                                            <MenuItem value="Global">Global</MenuItem>
+                                             <MenuItem value={user.house && user.house.house_id}> {user.house && user.house.name}</MenuItem>
+                                            <MenuItem value={0}>Global</MenuItem>
                                              </TextField>
                                     </Grid>
 
@@ -180,14 +180,14 @@ export default function Event_Create() {
                                         id="difficulty" 
                                         label="Difficulté" 
                                         style = {{width: 150}} 
-                                        defaultValue = "Facile"
-                                        required = "true" 
+                                        defaultValue = {1}
+                                        required = {true}
                                         { ...bindDifficulty }
                                         select>
-                                            <MenuItem value="Facile">Facile</MenuItem>
-                                            <MenuItem value="Moyen">Moyen</MenuItem>
-                                            <MenuItem value="Difficile">Difficile</MenuItem>
-                                            <MenuItem value="Extrême">Extrême</MenuItem>
+                                            <MenuItem value={1}>Facile</MenuItem>
+                                            <MenuItem value={2}>Moyen</MenuItem>
+                                            <MenuItem value={3}>Difficile</MenuItem>
+                                            <MenuItem value={4}>Extrême</MenuItem>
                                         </TextField>
                                     </Grid>
                                 </Grid>
@@ -197,15 +197,15 @@ export default function Event_Create() {
                                         <TextField 
                                             id="battleroyal" 
                                             label="Battle Royal Mode" 
-                                            required = "false" 
-                                            defaultValue = "Non"
+                                            required = {false} 
+                                            defaultValue = {0}
                                             disabled = { false }
                                             helperText = "*chacun pour soi."
                                             style = {{width: 150}} 
                                             { ...bindBattleRoyal }
                                             select>
-                                                <MenuItem value="Non">Non</MenuItem>
-                                                <MenuItem value="Oui">Oui</MenuItem>
+                                                <MenuItem value={0}>Non</MenuItem>
+                                                <MenuItem value={1}>Oui</MenuItem>
                                         </TextField>
                                     </Grid>
                                 </Grid>
@@ -217,8 +217,8 @@ export default function Event_Create() {
                                             label="Nb. min participants"
                                             placeholder="Veuillez indiquer le nombre minimum de participants."
                                             variant="outlined"
-                                            defaultValue = "0"
-                                            required = "true" 
+                                            defaultValue = {0}
+                                            required = {true} 
                                             { ...bindAttendeesMin }
                                         />
                                     </Grid>
@@ -227,8 +227,8 @@ export default function Event_Create() {
                                     <TextField
                                             id="deadline_reservation"
                                             label="Date limite inscription"
-                                            type="datetime-local"
-                                            defaultValue= {values.someDate} //TODO -> mettre une default value qui prend la date/heure actuelle
+                                            type="date"
+                                            defaultValue= {new Date()} //TODO -> mettre une default value qui prend la date/heure actuelle
                                             className={classes.textField}
                                             InputLabelProps={{
                                             shrink: true,
@@ -245,8 +245,8 @@ export default function Event_Create() {
                                         label="Nb. max participants"
                                         placeholder="Veuillez indiquer le nombre maximum de participants."
                                         variant="outlined"
-                                        defaultValue = "0"
-                                        required = "true" 
+                                        defaultValue = {0}
+                                        required = {true}
                                         { ...bindAttendeesMax }
                                         />
                                     </Grid>
@@ -254,8 +254,8 @@ export default function Event_Create() {
                                     <TextField
                                             id="date_begin"
                                             label="Date et heure de l'événement"
-                                            type="datetime-local"
-                                            defaultValue="2020-05-24T10:30"
+                                            type="date"
+                                            defaultValue={new Date()}
                                             className={classes.textField}
                                             InputLabelProps={{
                                             shrink: true,
@@ -271,8 +271,8 @@ export default function Event_Create() {
                                             label="Tarif"
                                             placeholder="Veuillez indiquer le tarif éventuel de l'événement."
                                             variant="outlined"
-                                            defaultValue = "0"
-                                            required = "true"
+                                            defaultValue = {0}
+                                            required = {true}
                                             { ...bindPrice }
                                         />
                                     </Grid>
@@ -280,8 +280,8 @@ export default function Event_Create() {
                                         <TextField
                                             id="date_end"
                                             label="Date et heure de fin"
-                                            type="datetime-local"
-                                            defaultValue="2020-05-24T10:30" //TODO -> mettre une default value qui prend la date/heure actuelle
+                                            type="date"
+                                            defaultValue={new Date()}
                                             className={classes.textField}
                                             InputLabelProps={{
                                             shrink: true,
@@ -299,7 +299,7 @@ export default function Event_Create() {
                                         label="Lieu"
                                         placeholder="Lieu de l'événement"
                                         variant="outlined"
-                                        required = "true" 
+                                        required = {true}
                                         style = {{minWidth: 450}}
                                         { ...bindLocation }
                                     /> 
@@ -313,7 +313,7 @@ export default function Event_Create() {
                                         label="Rue"
                                         placeholder="Lieu de l'événement"
                                         variant="outlined"
-                                        required = "true" 
+                                        required = {true} 
                                         style = {{minWidth: 450}}
                                         { ...bindRoad }
                                     /> 
@@ -324,7 +324,7 @@ export default function Event_Create() {
                                             label="N° de rue"
                                             placeholder="Lieu de l'événement"
                                             variant="outlined"
-                                            required = "true" 
+                                            required = {true}
                                             style = {{minWidth: 100, width:100}}
                                             { ...bindRoadNumber }
                                         /> 
@@ -338,7 +338,7 @@ export default function Event_Create() {
                                         label="Code postal"
                                         placeholder="Lieu de l'événement"
                                         variant="outlined"
-                                        required = "true" 
+                                        required = {true}
                                         style = {{minWidth: 80}}
                                         { ...bindPostalCode }
                                     /> 
@@ -349,7 +349,7 @@ export default function Event_Create() {
                                             label="Ville"
                                             placeholder="Lieu de l'événement"
                                             variant="outlined"
-                                            required = "true" 
+                                            required = {true}
                                             style = {{minWidth: 150, width:350}}
                                             { ...bindTown }
                                         /> 
