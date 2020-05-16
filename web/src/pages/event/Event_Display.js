1bc5736f2b30148bfa304e7b526dbb8393d0e298
CosmicElodie <CosmicElodie@users.noreply.github.com>
2020-05-16 05:43:22 +0200

    Event display on point !

--- web/src/pages/event/Event_Display.js
+++ web/src/pages/event/Event_Display.js
@@ -87,7 +87,7 @@ export default function Event() {
     }
     
     const printAccountPointsButton = ( { attendees_min, nb_attendees, status } ) => {
-        if(attendees_min < nb_attendees && status === 'En cours' ) return <Button variant="contained" size="small" color="secondary" onClick={ handleAccountPointDialogClick }> Clôturer les points </Button>
+        if(attendees_min < nb_attendees && status === 'En attente' ) return <Button variant="contained" size="small" color="secondary" onClick={ handleAccountPointDialogClick }> Clôturer les points </Button>
     }
 
     const handleAccountPointDialogClick = () => setAccountPointDialogState({ is_open : true });
@@ -116,7 +116,6 @@ export default function Event() {
                                         printCancelOrModifyButton(user.access_level, user.user_id, "Annuler")
                                 }
 
-
                                 {/* Bouton "supprimer", uniquement visible pour l'admin/modo */}
                                 { printDeleteButton(user.access_level) }
 
@@ -126,14 +125,13 @@ export default function Event() {
                                     <TableHead>
                                         <TableRow>
                                             <TableCell>
-                                            <h2>{ current.description }</h2>
+                                            <h2>Description</h2>
                                             </TableCell>
                                         </TableRow>
                                     </TableHead>
                                     <TableBody>
                                         <TableCell align='justify'>
-                                        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-                                        </TableCell>
+                                        { current.description } </TableCell>
                                     </TableBody>
                                 </Table>
                                 
@@ -154,17 +152,13 @@ export default function Event() {
                                     <TableBody>
                                         <TableRow>
                                             <TableCell>
-                                               
+                                                { current.deadline_reservation }
                                             </TableCell>
                                             <TableCell>
-                                                <Moment format="YYYY/MM/DD HH:mm">
-                                                    { current.date_begin }
-                                                </Moment>
+                                                { current.date_begin }
                                             </TableCell>
                                             <TableCell>
-                                                <Moment format="YYYY/MM/DD HH:mm">
-                                                    { current.date_end }
-                                                </Moment>
+                                                { current.date_end }
                                             </TableCell>
                                         </TableRow>
                                     </TableBody>
@@ -229,6 +223,15 @@ export default function Event() {
                                 </Table>
                                 <br />
                                 <center><h2>Adresse</h2></center>
+                                <Table>
+                                    <TableBody>
+                                        <TableRow>
+                                            <TableCell align="center">
+                                                {current.address}
+                                            </TableCell>
+                                        </TableRow>
+                                    </TableBody>
+                                </Table>
                             </CardContent>
                         </Card>
             }
