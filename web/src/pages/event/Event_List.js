386df9d203a5a7b275cdbfbab7f9cfce3db30795
CosmicElodie <CosmicElodie@users.noreply.github.com>
2020-05-18 15:26:09 +0200

    Events annulés visibles seulement pour modo et admin. Par contre y'a un bug qui subsiste sur la pagination (la pagination comptabilise quand même les events annulés alors qu'ils ne sont pas visibles pour un user lambda

--- web/src/pages/event/Event_List.js
+++ web/src/pages/event/Event_List.js
@@ -1,25 +1,29 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import { EventContext } from '../../context/EventContext';
+import { MainContext } from '../../context/MainContext';
 
 import PropTypes from "prop-types";
 import Moment from 'react-moment';
 
-import {Button, 
+import {
+    Button,
     Card,
     CardContent,
-    CssBaseline, 
-    Grid, 
-    Table, 
-    TableBody, 
-    TableCell, 
-    TableHead, 
-    TableContainer, 
+    CssBaseline,
+    Grid,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableContainer,
     TablePagination,
     TableRow,
     TableSortLabel,
-    Paper} from '@material-ui/core';
+    Paper,
+    useRadioGroup
+} from '@material-ui/core';
 
-import {makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
 
 const useStyles = makeStyles(theme => ({
@@ -42,26 +46,26 @@ const useStyles = makeStyles(theme => ({
 
 function descendingComparator(a, b, orderBy) {
     if (b[orderBy] < a[orderBy]) {
-      return -1;
+        return -1;
     }
     if (b[orderBy] > a[orderBy]) {
-      return 1;
+        return 1;
     }
     return 0;
 }
-  
+
 function getComparator(order, orderBy) {
     return order === "desc"
-      ? (a, b) => descendingComparator(a, b, orderBy)
-      : (a, b) => -descendingComparator(a, b, orderBy);
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
 }
 
 function stableSort(array, comparator) {
     const stabilizedThis = array.map((el, index) => [el, index]);
     stabilizedThis.sort((a, b) => {
-      const order = comparator(a[0], b[0]);
-      if (order !== 0) return order;
-      return a[1] - b[1];
+        const order = comparator(a[0], b[0]);
+        if (order !== 0) return order;
+        return a[1] - b[1];
     });
     return stabilizedThis.map(el => el[0]);
 }
@@ -77,31 +81,30 @@ const headCells = [
     { id: 'location', numeric: false, disablePadding: false, label: 'Lieu' }
 ];
 
-function EnhancedTableHead(props) 
-{
+function EnhancedTableHead(props) {
     const { order, orderBy, onRequestSort } = props;
     const createSortHandler = property => event => {
-      onRequestSort(event, property);
+        onRequestSort(event, property);
     };
     return (
         <TableHead>
-          <TableRow>
-            {headCells.map(headCell => (
-              <TableCell
-                key={headCell.id}
-                align={"right"}
-                sortDirection={orderBy === headCell.id ? order : false}
-              >
-                <TableSortLabel
-                  active={orderBy === headCell.id}
-                  direction={orderBy === headCell.id ? order : "asc"}
-                  onClick={createSortHandler(headCell.id)}
-                >
-                  {headCell.label}
-                </TableSortLabel>
-              </TableCell>
-            ))}
-          </TableRow>
+            <TableRow>
+                {headCells.map(headCell => (
+                    <TableCell
+                        key={headCell.id}
+                        align={"right"}
+                        sortDirection={orderBy === headCell.id ? order : false}
+                    >
+                        <TableSortLabel
+                            active={orderBy === headCell.id}
+                            direction={orderBy === headCell.id ? order : "asc"}
+                            onClick={createSortHandler(headCell.id)}
+                        >
+                            {headCell.label}
+                        </TableSortLabel>
+                    </TableCell>
+                ))}
+            </TableRow>
         </TableHead>
     );
 }
@@ -116,7 +119,8 @@ EnhancedTableHead.propTypes = {
 
 export default function Event_List() {
     const classes = useStyles();
-    const {data} = useContext(EventContext);
+    const { user, setDialog } = useContext(MainContext);
+    const { data } = useContext(EventContext);
 
     const [order, setOrder] = React.useState("asc");
     const [orderBy, setOrderBy] = React.useState();
@@ -134,50 +138,67 @@ export default function Event_List() {
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
     };
-    
+
     const handleChangeRowsPerPage = event => {
         setRowsPerPage(parseInt(event.target.value, 10));
         setPage(0);
     };
 
-    Object.size = function(obj) {
+    Object.size = function (obj) {
         var size = 0, key;
         for (key in obj) {
             if (obj.hasOwnProperty(key)) size++;
         }
         return size;
     };
-    
-    function displayRightHouse(noHouse)
-    {
-        if(noHouse == null)
-        {
-            return "Globale";
-        }
-        switch(noHouse)
-        {
-            case 1 : return "IE";
-            case 2 : return "TS";
-            case 3 : return "TR";
-            case 4 : return "IL";
-            case 5 : return "ID";
-            default : return "error";
+
+
+
+    function displayEveryEventExceptCancelledOnes(index, event_id, name, description, is_competitive, difficulty, battleroyale,
+        status, price, attendees_min, attendees_max, created, deadline_reservation,
+        date_begin, date_end, location, address, house, organisator, participants, nb_attendees) {
+        if (user.access_level < 50 && status != "Annulé") {
+            return (<TableRow tabIndex={-1}>
+                <TableCell align="right"
+                    component="th"
+                    id={`enhanced-table-checkbox-${index}`}
+                    scope="row">{name}
+                </TableCell>
+                <TableCell align="right">
+                    {organisator.firstname + ' ' + organisator.lastname}
+                </TableCell>
+
+                <TableCell align="right"> {house == null ? "Global" : house.shortname}</TableCell>
+                <TableCell align="right">{nb_attendees + ' / ' + attendees_max}</TableCell>
+                <TableCell align="right">
+                    <Moment format="YYYY/MM/DD HH:mm">
+                        {deadline_reservation}
+                    </Moment>
+                </TableCell>
+                <TableCell align="right">
+                    <Moment format="YYYY/MM/DD HH:mm">
+                        {date_begin}
+                    </Moment>
+                </TableCell>
+                <TableCell align="right">{location}</TableCell>
+            </TableRow>
+            );
         }
     }
-    
+
     return (
         <React.Fragment>
-            <CssBaseline/>
+            <CssBaseline />
             <main>
                 <Grid>
                     <Card className={classes.card}>
                         <CardContent className={classes.cardContent}>
                             <center><h1>Liste des événements</h1></center>
                             <TableContainer component={Paper}>
-                                <Table  className={classes.table}
-                                        aria-labelledby="tableTitle"
-                                        size={dense ? "small" : "medium"}
-                                        aria-label="enhanced table"
+                                <Table className={classes.table}
+                                    aria-labelledby="tableTitle"
+                                    size={dense ? "small" : "medium"}
+                                    aria-label="enhanced table"
                                 >
                                     <EnhancedTableHead
                                         classes={classes}
@@ -188,37 +209,16 @@ export default function Event_List() {
                                         rowCount={7}
                                     />
                                     <TableBody>
-                                    {data && stableSort(data, getComparator(order, orderBy))
-                                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                        .map(({     event_id, name, description, is_competitive, difficulty, battleroyale, 
-                                            status, price, attendees_min, attendees_max, created, deadline_reservation, 
-                                            date_begin, date_end, location, address, house, organisator, participants, nb_attendees }, index) =>
-                                                    
-                                                <TableRow tabIndex={-1}>
-                                                    <TableCell align="right" 
-                                                        component="th"
-                                                        id={`enhanced-table-checkbox-${index}`}
-                                                        scope="row">{name}
-                                                    </TableCell>
-                                                    <TableCell align="right">
-                                                        {organisator.firstname + ' ' + organisator.lastname}
-                                                    </TableCell>
-                                                    
-                                                    <TableCell align="right"> { house == null ? "Global" : house.shortname}</TableCell>
-                                                    <TableCell align="right">{nb_attendees + ' / ' + attendees_max}</TableCell>
-                                                    <TableCell align="right">
-                                                        <Moment format="YYYY/MM/DD HH:mm">
-                                                            {deadline_reservation}
-                                                        </Moment>
-                                                    </TableCell>
-                                                    <TableCell align="right">
-                                                        <Moment format="YYYY/MM/DD HH:mm">
-                                                            {date_begin}
-                                                        </Moment>
-                                                    </TableCell>
-                                                    <TableCell align="right">{location}</TableCell>
-                                                </TableRow>
-                                    )}
+                                        {data && stableSort(data, getComparator(order, orderBy))
+                                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                            .map(({ event_id, name, description, is_competitive, difficulty, battleroyale,
+                                                status, price, attendees_min, attendees_max, created, deadline_reservation,
+                                                date_begin, date_end, location, address, house, organisator, participants, nb_attendees }, index) =>
+
+                                                displayEveryEventExceptCancelledOnes(index, event_id, name, description, is_competitive, difficulty, battleroyale,
+                                                    status, price, attendees_min, attendees_max, created, deadline_reservation,
+                                                    date_begin, date_end, location, address, house, organisator, participants, nb_attendees)
+                                            )}
                                     </TableBody>
                                 </Table>
                             </TableContainer>
