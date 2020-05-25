c0cbc96c7d88e35eb8f5fa9a4727ce150c1d034a
MikeDTr <michael.triponez@heig-vd.ch>
2020-05-25 09:07:43 +0200

    "changed old spreadsheet to better modules"

--- web/src/pages/Home.js
+++ web/src/pages/Home.js
@@ -5,6 +5,8 @@ import { EventContext } from '../context/EventContext';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import PropTypes from "prop-types";
+import MUIDataTable from "mui-datatables";
+import moment from 'moment';
 import {
     Button,
     Card, CardContent, CardMedia,
@@ -46,7 +48,23 @@ const useStyles = makeStyles(theme => ({
     }
 }));
 
+
+   const columns = [
+    { name: 'name', label: 'Nom' , options: {filter: false, sort: true,}},
+    { name: 'organisator',  label: 'Organisateur' , options: {filter: false, sort: true,}},
+    { name: 'limitation', label: 'Limitation', options: {filter: true, sort: true,} },
+    { name: 'nb_attendees',label: 'Nb participants', options: {filter: false, sort: true,} },
+    { name: 'deadline_reservation',label: 'Date limite inscription', options: {filter: false, sort: true,} },
+    { name: 'date_begin', label: 'Date', options: {filter: false, sort: true,} },
+    { name: 'location',label: 'Lieu', options: {filter: false, sort: true,} },
+    { name: 'status',  label: 'Statut', options: {filter: true, sort: true,} },
+    { name: 'event_id',  label: 'event', options: {viewColumns : false, filter: false,display: false,} }
+   ];
+
+   
+
 export default function Home() {
+    
     const { user } = useContext(MainContext);
 
     //const {user} = useContext(MainContext);
@@ -60,28 +78,35 @@ export default function Home() {
         history.push(link);
     });
 
-
-    const [order, setOrder] = React.useState("asc");
-    const [orderBy, setOrderBy] = React.useState();
-    const [selected] = React.useState([]);
-    const [page, setPage] = React.useState(0);
-    const [dense] = React.useState(false);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
-
-    const handleRequestSort = (event, property) => {
-        const isAsc = orderBy === property && order === "asc";
-        setOrder(isAsc ? "desc" : "asc");
-        setOrderBy(property);
-    };
-
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-    };
-
-    const handleChangeRowsPerPage = event => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0);
-    };
+    var reformatData = function(data) {
+        return data.map(function(data) {
+          // create a new object to store full name.
+          var newObj = {};
+          newObj["event_id"] = data.event_id
+          newObj["name"] = data.name
+          newObj["status"] = data.status
+          newObj["deadline_reservation"] = moment(data.deadline_reservation).format('DD/MM/YYYY HH:mm')
+          newObj["date_begin"] =  moment(data.date_begin).format('DD/MM/YYYY HH:mm')
+          newObj["location"] = data.location
+          newObj["limitation"] = data.house == null ? "Global" : data.house.shortname
+          newObj["organisator"] = data.organisator.firstname + ' ' + data.organisator.lastname
+          newObj["nb_attendees"] = data.nb_attendees + ' / ' + data.attendees_max
+          // return our new object.
+          return newObj;
+        });
+      };
+    
+      const options = {     
+            
+        filterType: 'checkbox',
+        print: "",
+        selectableRows: 'none',
+        onRowClick: (rowData, rowState) => {
+            let _id = rowData[8]
+            console.log(rowData);
+            redirectPage("/event_display/" + _id)
+        },
+      };
 
     Object.size = function (obj) {
         var size = 0, key;
@@ -130,208 +155,6 @@ export default function Home() {
             .then(response => { setJoinedEvents(response) })
     }
 
-    function displayLine(index, event_id, name, date_begin, location, nb_attendees, status) {
-        return (<TableRow tabIndex={-1}>
-            <TableCell align="right"
-                component="th"
-                id={`enhanced-table-checkbox-${index}`}
-                scope="row">
-                <Button
-                    onClick={() => redirectPage("/event_display/" + event_id)}>
-                    {name}
-                </Button>
-            </TableCell>
-            <TableCell align="left"><Moment format="DD/MM/YYYY - HH:mm">{date_begin}</Moment></TableCell>
-            <TableCell align="left">{location}</TableCell>
-            <TableCell align="left">{nb_attendees}</TableCell>
-            <TableCell align="left">{status}</TableCell>
-        </TableRow>
-        );
-    }
-
-    const displayCreatedEvents = (name, events) => {
-        return (
-
-            <Card className={classes.card}>
-                <center><h2>{name}</h2></center>
-                <CardMedia
-                    className={classes.cardMedia}
-                    image=""
-                    title="Image title"
-                />
-
-                <CardContent className={classes.cardContent}>
-                    <Typography>
-                        <TableContainer component={Paper}>
-                            <Table className={classes.table}
-                                aria-labelledby="tableTitle"
-                                size={dense ? "small" : "medium"}
-                                aria-label="enhanced table"
-                            >
-                                <EnhancedTableHead
-                                    classes={classes}
-                                    numSelected={selected.length}
-                                    order={order}
-                                    orderBy={orderBy}
-                                    onRequestSort={handleRequestSort}
-                                    rowCount={7}
-                                />
-                                <TableBody>
-                                    {createdEvents && createdEvents.length > 0 && stableSort(createdEvents, getComparator(order, orderBy))
-                                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(({ index, event_id, name, description, is_competitive, difficulty, battleroyale,
-                                            status, price, attendees_min, attendees_max, created, deadline_reservation,
-                                            date_begin, date_end, location, address, house, organisator, participants, nb_attendees }) =>
-
-                                            displayLine(index, event_id, name, date_begin, location, nb_attendees, status)
-                                        )}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                        <TablePagination
-                            rowsPerPageOptions={[5, 10, 25]}
-                            component="div"
-                            count={Object.size(createdEvents)}
-                            rowsPerPage={rowsPerPage}
-                            page={page}
-                            onChangePage={handleChangePage}
-                            onChangeRowsPerPage={handleChangeRowsPerPage}
-                        />
-                    </Typography>
-                </CardContent>
-            </Card>
-        )
-    }
-
-    const displayJoinedEvents = (name, events) => {
-        return (
-
-            <Card className={classes.card}>
-                <center><h2>{name}</h2></center>
-
-                <CardMedia
-                    className={classes.cardMedia}
-                    image=""
-                    title="Image title"
-                />
-
-                <CardContent className={classes.cardContent}>
-                    <Typography>
-                        <TableContainer component={Paper}>
-                            <Table className={classes.table}
-                                aria-labelledby="tableTitle"
-                                size={dense ? "small" : "medium"}
-                                aria-label="enhanced table"
-                            >
-
-                                <EnhancedTableHead
-                                    classes={classes}
-                                    numSelected={selected.length}
-                                    order={order}
-                                    orderBy={orderBy}
-                                    onRequestSort={handleRequestSort}
-                                    rowCount={7}
-                                />
-
-                                <TableBody>
-                                    {joinedEvents && joinedEvents.length > 0 && stableSort(joinedEvents, getComparator(order, orderBy))
-                                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(({ index, event_id, name, description, is_competitive, difficulty, battleroyale,
-                                            status, price, attendees_min, attendees_max, created, deadline_reservation,
-                                            date_begin, date_end, location, address, house, organisator, participants, nb_attendees }) =>
-
-                                            displayLine(index, event_id, name, date_begin, location, nb_attendees, status)
-                                        )}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                        <TablePagination
-                            rowsPerPageOptions={[5, 10, 25]}
-                            component="div"
-                            count={Object.size(joinedEvents)}
-                            rowsPerPage={rowsPerPage}
-                            page={page}
-                            onChangePage={handleChangePage}
-                            onChangeRowsPerPage={handleChangeRowsPerPage}
-                        />
-                    </Typography>
-                </CardContent>
-            </Card>
-
-        )
-    }
-
-    function EnhancedTableHead(props) {
-        const { order, orderBy, onRequestSort } = props;
-        const createSortHandler = property => event => {
-            onRequestSort(event, property);
-        };
-
-        return (
-            <TableHead>
-                <TableRow>
-                    {headCells.map(headCell => (
-                        <TableCell
-                            key={headCell.id}
-                            align={"left"}
-                            sortDirection={orderBy === headCell.id ? order : false}
-                        >
-                            <TableSortLabel
-                                active={orderBy === headCell.id}
-                                direction={orderBy === headCell.id ? order : "asc"}
-                                onClick={createSortHandler(headCell.id)}
-                            >
-                                {headCell.label}
-                            </TableSortLabel>
-                        </TableCell>
-                    ))}
-                </TableRow>
-            </TableHead>
-        );
-    }
-
-    function descendingComparator(a, b, orderBy) {
-        if (b[orderBy] < a[orderBy]) {
-            return -1;
-        }
-        if (b[orderBy] > a[orderBy]) {
-            return 1;
-        }
-        return 0;
-    }
-
-    function getComparator(order, orderBy) {
-        return order === "desc"
-            ? (a, b) => descendingComparator(a, b, orderBy)
-            : (a, b) => -descendingComparator(a, b, orderBy);
-    }
-
-    function stableSort(array, comparator) {
-        const stabilizedThis = array.map((el, index) => [el, index]);
-        stabilizedThis.sort((a, b) => {
-            const order = comparator(a[0], b[0]);
-            if (order !== 0) return order;
-            return a[1] - b[1];
-        });
-        return stabilizedThis.map(el => el[0]);
-    }
-
-    //Titre des colonnes
-    const headCells = [
-        { id: 'name', numeric: false, disablePadding: false, label: 'Nom' },
-        { id: 'date_begin', numeric: true, disablePadding: false, label: 'Date début' },
-        { id: 'location', numeric: false, disablePadding: false, label: 'Lieu' },
-        { id: 'nb_attendees', numeric: true, disablePadding: false, label: 'Nb participants' },
-        { id: 'status', numeric: false, disablePadding: false, label: 'Statut' }
-    ];
-
-
-
-    EnhancedTableHead.propTypes = {
-        classes: PropTypes.object.isRequired,
-        onRequestSort: PropTypes.func.isRequired,
-        order: PropTypes.oneOf(["asc", "desc"]).isRequired,
-        orderBy: PropTypes.string.isRequired,
-        rowCount: PropTypes.number.isRequired
-    };
 
     return (
         <React.Fragment>
@@ -341,11 +164,28 @@ export default function Home() {
                 <Grid spacing={2} container direction="row" justify="space-evenly" alignItems="stretch">
 
                     <Grid item xs>
-                        {displayJoinedEvents('Événements rejoints par ' + user.firstname, joinedEvents)}
+                        {/*displayJoinedEvents('Événements rejoints par ' + user.firstname, joinedEvents)*/}
+                        <center><h1>Événements rejoints </h1></center>
+                        {
+                            joinedEvents && <MUIDataTable
+                            data={reformatData(joinedEvents)}
+                            columns={columns}
+                            options={options}
+                            />
+                        }
                     </Grid>
 
                     <Grid item xs>
-                        {displayCreatedEvents('Événements créés par ' + user.firstname, createdEvents)}
+                        {/*displayCreatedEvents('Événements créés par ' + user.firstname, createdEvents)*/}
+                        <center><h1>Événements créés </h1></center>
+                        {
+                            createdEvents && <MUIDataTable
+                            title={"Événements créés par"}
+                            data={reformatData(createdEvents)}
+                            columns={columns}
+                            options={options}
+                            />
+                        }
                     </Grid>
 
                 </Grid>
