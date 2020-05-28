0906ebe368d65bbb34184e3a93a8c2b4abc6d281
Ovich <stefanteofanovic@hotmail.com>
2020-05-28 17:57:50 +0200

    Fix Account Point Forum : Auto Close

--- web/src/pages/event/EventAccountPoints.js
+++ web/src/pages/event/EventAccountPoints.js
@@ -38,7 +38,7 @@ const EventAccountPoints = ({ is_open, handleClose, event_id, difficulty, battle
                     is_open: true
                 }
             });
-            if(status === 'success') handleClose();
+            if(status === 'ok') handleClose();
         });  
     }
 
