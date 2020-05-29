86433a1c8f426bd2f9161ca1ad3f508bfc74776d
MikeDTr <michael.triponez@heig-vd.ch>
2020-05-29 03:18:46 +0200

    data added to the shown profile

--- web/src/layout/ShowProfile.js
+++ web/src/layout/ShowProfile.js
@@ -90,7 +90,8 @@ const ShowProfile = () => {
                               <DisplayData name="Nom :" data = {showProfile.lastname}/>    
                               <DisplayData name="Prénom :" data = {showProfile.firstname}/>    
                               <DisplayDate name="Naissance :" data = {showProfile.birth}/>                             
-                              <DisplayData name="Points :" data = {showProfile.points_year}/>     
+                              <DisplayData name="Points :" data = {showProfile.points_year}/>  
+                              <DisplayData name="Forum post :" data = {showProfile.nb_subject_participate }/>  
                               <DisplayData name="Maison :" data = {showProfile.house && showProfile.house.name}/>    
                               <DisplayData name="E-mail :" data = {showProfile.email}/>    
                             </Typography>   
